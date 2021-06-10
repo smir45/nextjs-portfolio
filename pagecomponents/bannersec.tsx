a127a962dbc122403d19a699c81f1d74efff45d5
Samir Mishra <smir.mishra1551@gmail.com>
2021-06-10 22:34:17 +0545

    bug fixed

--- pagecomponents/bannersec.tsx
+++ pagecomponents/bannersec.tsx
@@ -1,10 +1,8 @@
 import styles from "../styles/bodysec.module.scss";
 import Link from "next/link";
 const BannerSection = () => {
-  
   return (
     <main className={styles.elementContainer}>
-      
       <div className={styles.textSection}>
         <div className={styles.textinner}>
           <h1 className={styles.titletext}>
@@ -23,6 +21,9 @@ const BannerSection = () => {
         </div>
       </div>
       <div className={styles.svgSection}>
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
         <div className={styles.svgbox}></div>
       </div>
     </main>
