14eaafe0f4a28e225e9f239d865e979ae805ac6f
Samir Mishra <smir.mishra1551@gmail.com>
2021-07-04 23:54:55 +0545

    making changes

--- pagecomponents/bannersec.tsx
+++ pagecomponents/bannersec.tsx
@@ -4,28 +4,8 @@ import Link from "next/link";
 const BannerSection = () => {
   return (
     <main className={styles.elementContainer}>
-      <div className={styles.textSection}>
-        <div className={styles.textinner}>
-          <h1 className={styles.titletext}>
-            Hi,
-            <br />
-            I'm'
-            <br />
-            Samir Mishra
-          </h1>
-          <p className={styles.paragraphTitle}>
-            Full Stack Webdeveloper / Freelancer and a Student
-          </p>
-          <Link href="/contact">
-            <a className={styles.contactbtn}>Contact</a>
-          </Link>
-        </div>
-      </div>
-      <div className={styles.svgSection}>
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
-        <div className={styles.svgbox}></div>
+      <div className={styles.mainContainerBanner}>
+        trial
       </div>
     </main>
   );
