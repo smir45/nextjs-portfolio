fa299f578c2630f8dcefec7ea275591725173ec4
Samir Mishra <smir.mishra1551@gmail.com>
2021-07-04 22:34:33 +0545

    modifying portfolio and redesigning it

--- pagecomponents/bannersec.tsx
+++ pagecomponents/bannersec.tsx
@@ -1,5 +1,6 @@
 import styles from "../styles/bodysec.module.scss";
 import Link from "next/link";
+// main section
 const BannerSection = () => {
   return (
     <main className={styles.elementContainer}>
